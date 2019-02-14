b7455b8b32e9c6628f8dc860b8e637847610f49c
ljkimura <lifina07@me.com>
2019-02-14 09:04:36 +0900

    fix publish

--- main.go
+++ main.go
@@ -14,30 +14,29 @@ import (
 
 const projectID = "positive-apex-202905"
 
-func publish(ctx context.Context, topic Topic) (string, error) {
+func publish(ctx context.Context, topic Topic) error {
 	client, err := pubsub.NewClient(ctx, projectID)
 	if err != nil {
 		log.Errorf(ctx, "Failed to create the pubsub client %v", err)
-		return "", err
+		return err
 	}
 
 	baseDate := time.Date(2019, 2, 1, 0, 0, 0, 0, time.Local)
 	now := time.Now()
 	h := math.Trunc(now.Sub(baseDate).Hours())
 	d := int64(math.Ceil(h / 24))
+	message := fmt.Sprintf("無職%d日目", d)
 
 	t := client.TopicInProject(string(topic), projectID)
-	defer t.Stop()
-	result := t.Publish(ctx, &pubsub.Message{
-		Data: []byte(fmt.Sprintf("無職%d日目", d)),
-	})
-
-	id, err := result.Get(ctx)
+	_, err = t.Publish(ctx, &pubsub.Message{
+		Data: []byte(message),
+	}).Get(ctx)
 	if err != nil {
 		log.Errorf(ctx, "Failed to publish a message %v", err)
-		return "", err
+		return err
 	}
-	return id, nil
+	log.Debugf(ctx, "Successed to publish a message %v", message)
+	return nil
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
@@ -47,10 +46,10 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 func publishHandler(w http.ResponseWriter, r *http.Request) {
 	if topic, found := strToTopic[r.URL.Query().Get("topic")]; found {
 		ctx := appengine.NewContext(r)
-		if id, err := publish(ctx, topic); err != nil {
+		if err := publish(ctx, topic); err != nil {
 			http.Error(w, "internal", http.StatusInternalServerError)
 		} else {
-			fmt.Fprintf(w, "published: %v", id)
+			fmt.Fprintf(w, "published")
 		}
 	} else {
 		http.Error(w, "bad topic", http.StatusBadRequest)
